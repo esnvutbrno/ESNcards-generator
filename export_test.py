54c3f3150d49e07fc17e44c1bd63db0a679c1906
Jozef Zuzelka <jozef.zuzelka@gmail.com>
2021-02-11 16:45:56 +0100

    Added possibility of reversed print

--- export_test.py
+++ export_test.py
@@ -24,6 +24,10 @@ class PrintMode(IntEnum):
     TEXT_ONLY = 2   # Printing only descriptions to transparent foil
     ALL = 3         # Printing everything with classical layout
 
+class PrintOrder(IntEnum):
+    NORMAL = 1     # Photos (and text) are being placed from the top to the bottom of the page
+    REVERSED = 2   # Photos (and text) are being placed from the bottom to the top
+
 class A4Size:
     """A4 paper size"""
     w = 210
@@ -46,36 +50,38 @@ class CardSpacing:
     dateDelta = 32      # space between text column and date column
     dayDelta = 5.5      # space between parts of the date
 
-
 class TextDeltas:
-    """Distance from initial photo position (0,0)"""
-    xName = CardSpacing.textDelta
-    yName = CardSpacing.rowDelta
+    """Distances from initial photo position (0,0)"""
+    xName = 0
+    yName = 0
 
-    xNationality = CardSpacing.textDelta
+    xNationality = xName
     yNationality = yName + CardSpacing.rowDelta
 
-    xBirthday = CardSpacing.textDelta + CardSpacing.dateDelta
+    xBirthday = xNationality + CardSpacing.dateDelta
     yBirthday = yNationality
 
-    xFaculty = CardSpacing.textDelta
+    xFaculty = xName
     yFaculty = yBirthday + CardSpacing.rowDelta
 
-    xSection = CardSpacing.textDelta
+    xSection = xName
     ySection = yFaculty + CardSpacing.rowDelta
 
-    xValidity = CardSpacing.textDelta + CardSpacing.dateDelta
+    xValidity = xSection + CardSpacing.dateDelta
     yValidity = ySection
-
 
 class ContentSpacing:
     """The most efficient spacing based on printing mode selected"""
-    xBorder = 5
-    yBorder = 5
+    xLeftLimit = 0      # Topmost X coordinate
+    yTopLimit = 0       # Topmost Y coordinate
+    xRightLimit = 0     # Lowermost possible X coordinate
+    yBottomLimit = 0    # Lowermost possible Y coordinate
+    xBorder = 5         # Border on each side of the page
+    yBorder = 5         # Border on the top and the bottom if the page
     xIncrement = 0
     yIncrement = 0
 
-    def __init__(self, mode):
+    def __init__(self, mode, order):
         if mode == PrintMode.PHOTO_ONLY:
             self.xIncrement = PhotoSize.w + (PhotoSize.w >> 3) # Photo width + 12.5% for spacing
             self.yIncrement = PhotoSize.h + (PhotoSize.h >> 3) # Photo height + 12.5% for spacing
@@ -87,17 +93,27 @@ class ContentSpacing:
             self.xIncrement = PhotoSize.w + 6 + TextSize.w + 2 # Photo width + 6mm space + Text width + print spacing
             self.yIncrement = PhotoSize.h + (PhotoSize.h >> 3) # Photo height + 12.5% for spacing
 
+        if order == PrintOrder.REVERSED:
+            self.xIncrement *= -1
+            self.yIncrement *= -1
+
+        self.xLeftLimit = self.xBorder
+        self.yTopLimit = self.yBorder
+        self.xRightLimit = A4Size.w - self.xBorder - abs(self.xIncrement)
+        self.yBottomLimit = A4Size.h - self.yBorder - abs(self.yIncrement)
+
 class Config:
     imgextensions = ('.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff')
-    imgpath = ""
-    peoplecsv = ""
-    output = ""
+    imgpath = "/tmp/ESNcard/imgs/"
+    peoplecsv = "/tmp/ESNcard/people.csv"
+    output = "/tmp/ESNcard/images.pdf"
     spacing = None
-    mode = None
+    mode = PrintMode.ALL
+    order = PrintOrder.NORMAL
 
     @staticmethod
     def info():
-        return f"imgpath={Config.imgpath}, peoplecsv={Config.peoplecsv}, output={Config.output}, imgextensions={Config.imgextensions}, spacing:{Config.spacing.xBorder, Config.spacing.yBorder, Config.spacing.xIncrement, Config.spacing.yIncrement}"
+        return f"imgpath={Config.imgpath}, peoplecsv={Config.peoplecsv}, output={Config.output}, imgextensions={Config.imgextensions}, mode: {Config.mode}, order: {Config.order}, spacing:{Config.spacing.xBorder, Config.spacing.yBorder, Config.spacing.xIncrement, Config.spacing.yIncrement}"
 
 class PersonInfo:
     name = ""
@@ -109,10 +125,11 @@ class PersonInfo:
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--imgpath', default="/tmp/ESNcard/imgs/", help='folder with images to be processed (default: \'/tmp/ESNcard/imgs/\')')
-    parser.add_argument('--peoplecsv', default="/tmp/ESNcard/people.csv", help='CSV file with students and their details (default: \'/tmp/ESNcard/people.csv\')')
-    parser.add_argument('--output', default="/tmp/ESNcard/images.pdf", help='Output file (default: \'/tmp/ESNcard/images.pdf\')')
-    parser.add_argument('--mode', type=int, default=f"{PrintMode.ALL}", help=f'Printing mode\n\t{PrintMode.PHOTO_ONLY} - Photo only,\t{PrintMode.TEXT_ONLY} - Text only,\t{PrintMode.ALL} - All (default: {PrintMode.ALL})')
+    parser.add_argument('--imgpath', help=f'folder with images to be processed (default: \'{Config.imgpath}\')')
+    parser.add_argument('--peoplecsv', help=f'CSV file with students and their details (default: \'{Config.peoplecsv}\')')
+    parser.add_argument('--output', help=f'Output file (default: \'{Config.output}\')')
+    parser.add_argument('--mode', type=int, help=f'Printing mode\n\t{PrintMode.PHOTO_ONLY} - Photo only,\t{PrintMode.TEXT_ONLY} - Text only,\t{PrintMode.ALL} - All (default: {Config.mode})')
+    parser.add_argument('--order', type=int, help=f'Printing order\n\t{PrintOrder.NORMAL} - TOP -> BOTTOM,\t{PrintOrder.REVERSED} - BOTTOM -> TOP (default: {Config.order})')
 
     args, rest = parser.parse_known_args()
     sys.argv = sys.argv[:1] + rest
@@ -120,11 +137,22 @@ def parse_args():
     if args.mode < 1 or args.mode > 3:
         raise ValueError("Invalid mode specified.")
 
-    Config.imgpath = args.imgpath + "/"
-    Config.peoplecsv = args.peoplecsv
-    Config.output = args.output
-    Config.spacing = ContentSpacing(args.mode)
-    Config.mode = args.mode
+    if args.imgpath is not None:
+        Config.imgpath = args.imgpath + "/"
+
+    if args.peoplecsv is not None:
+        Config.peoplecsv = args.peoplecsv
+
+    if args.output is not None:
+        Config.output = args.output
+
+    if args.mode is not None:
+        Config.mode = args.mode
+
+    if args.order is not None:
+        Config.order = args.order
+
+    Config.spacing = ContentSpacing(Config.mode, Config.order)
 
 def load_images():
     try:
@@ -134,9 +162,7 @@ def load_images():
         sys.exit(1)
 
 def print_person_info(pdf, x, y, pi):
-    # By default, TextDeltas count with photo width so substract it to get correct init position for text in case we are not printing photos
-    if Config.mode == PrintMode.TEXT_ONLY:
-        x -= PhotoSize.w
+    pdf.set_font_size(8)
 
     pdf.text(x + TextDeltas.xName,          y + TextDeltas.yName, pi.name)
     pdf.text(x + TextDeltas.xNationality,   y + TextDeltas.yNationality, pi.nationality)
@@ -163,11 +189,7 @@ def print_person_info(pdf, x, y, pi):
              y + TextDeltas.yValidity,
              pi.validity.strftime("%y")) # year
 
-
-def do():
-    # TODO try-catch
-
-    pdf = FPDF('P', 'mm', 'A4')
+def page_setup(pdf):
     pdf.add_font("NotoSans", style="", fname="NotoSans-Regular.ttf", uni=True)
     pdf.add_font("NotoSans", style="B", fname="NotoSans-Bold.ttf", uni=True)
     pdf.add_font("NotoSans", style="I", fname="NotoSans-Italic.ttf", uni=True)
@@ -176,14 +198,29 @@ def do():
 
     pdf.add_page()
 
-    xBorder = Config.spacing.xBorder
-    yBorder = Config.spacing.yBorder
+def do():
+    # TODO try-catch
+    pdf = FPDF('P', 'mm', 'A4')
+    page_setup(pdf)
+
+    xLeftLimit = Config.spacing.xLeftLimit
+    yTopLimit = Config.spacing.yTopLimit
+    xRightLimit = Config.spacing.xRightLimit
+    yBottomLimit = Config.spacing.yBottomLimit
     xIncrement = Config.spacing.xIncrement
     yIncrement = Config.spacing.yIncrement
-    x,y,w,h = xBorder, yBorder, PhotoSize.w, PhotoSize.h
-    i = 0
+
+    if Config.order == PrintOrder.NORMAL:
+        xInit= xLeftLimit
+        yInit = yTopLimit
+    else:
+        xInit = xRightLimit
+        yInit = yBottomLimit
+
+    x,y = xInit, yInit
 
     # For debug message only.
+    i = 0
     with open(Config.peoplecsv) as f:
         rows = sum(1 for line in f)-1
 
@@ -195,38 +232,45 @@ def do():
             pi = PersonInfo()
             pi.name = row["name"]
             pi.nationality = row["country"]
-            pi.birthday = date(int("20" + row["Y0"] + row["Y1"]), int(row["M0"] + row["M1"]), int(row["D0"] + row["D1"]))
-            pi.validity = date(int("20" + row["TY0"] + row["TY1"]), int(row["TM0"] + row["TM1"]), int(row["TD0"] + row["TD1"])) # FIXME fix the dirty year hack
+            pi.birthday = date(int("20" + row["Y0"] + row["Y1"]), int(row["M0"] + row["M1"]), int(row["D0"] + row["D1"]))       # FIXME fix the dirty year hack (20xx)
+            pi.validity = date(int("20" + row["TY0"] + row["TY1"]), int(row["TM0"] + row["TM1"]), int(row["TD0"] + row["TD1"])) # FIXME fix the dirty year hack (20xx)
 
             logger.debug(f"Exporting ({i}/{rows}) {pi.name}")
 
             if Config.mode != PrintMode.TEXT_ONLY:
-                foundImgs = [f for f in os.listdir(Config.imgpath) if re.match(rf"{pi.name}*", f) and any(f.endswith(ext) for ext in Config.imgextensions)]
+                foundImgs = [f for f in os.listdir(Config.imgpath) if re.match(rf"{pi.name}.*", f) and any(f.endswith(ext) for ext in Config.imgextensions)]
                 logger.debug(f"Matched photos: {foundImgs}")
 
-                pdf.image(Config.imgpath + foundImgs[0], x, y, w, h)
+                pdf.image(Config.imgpath + foundImgs[0], x, y, PhotoSize.w, PhotoSize.h)
 
                 # Write name below the image
-                pdf.set_font_size(6)
                 xText = x
-                yText = y + PhotoSize.h + 2
+                yText = y + PhotoSize.h + 2 # photo height + spacing
+
+                pdf.set_font_size(6)
                 pdf.text(xText, yText, pi.name)
 
             if Config.mode != PrintMode.PHOTO_ONLY:
-                pdf.set_font_size(8)
-                print_person_info(pdf, x, y, pi)
+                xText, yText = x, y
+
+                # If we print both photo and text, move init position of text next to the photo
+                if Config.mode != PrintMode.TEXT_ONLY:
+                    xText += CardSpacing.textDelta
+                    yText += CardSpacing.rowDelta
+
+                print_person_info(pdf, xText, yText, pi)
 
             x += xIncrement
 
-            # check for need to increment row
-            if x >= A4Size.w - xBorder - xIncrement:
-                x = xBorder
+            # check for need to increment/decrement row
+            if x < xLeftLimit or x > xRightLimit:
+                x = xInit
                 y += yIncrement
 
             # check if a new page should be added
-            if y >= A4Size.h - yBorder - yIncrement:
+            if y < yTopLimit or y > yBottomLimit:
                 logger.debug(f"Height limit reached. Adding a new page.")
-                y = yBorder
+                y = yInit
                 pdf.add_page()
 
     pdf.output(Config.output, "F")
